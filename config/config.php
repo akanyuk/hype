2695a7938085eac760a79fa68c1ba4c9c3aa2f09
nyuk <aka.nyuk@gmail.com>
2021-01-16 22:38:06 +0300

    `img_resize_width` increased

--- config/config.php
+++ config/config.php
@@ -24,7 +24,7 @@ $config['view']['description'] = 'demoscene hype'; // seo description
 $config['view']['keywords']    = 'demoscene, oldschool, zx-spectrum';      // seo keywords
 $config['view']['tinymce']         = false;  // использовать или нет визуальный редактор TinyMCE
 $config['view']['noindex']          = true;   // "прятать" или нет ссылки от поисковиков, оборачивая их в тег <noindex> и добавляя rel="nofollow"
-$config['view']['img_resize_width'] = 570;    // до какого размера в пикселях ужимать картинку по щирине при загрузки её в топики и комменты
+$config['view']['img_resize_width'] = 640;    // до какого размера в пикселях ужимать картинку по щирине при загрузки её в топики и комменты
 $config['view']['img_max_width'] = 5000;    // максимальная ширина загружаемых изображений в пикселях
 $config['view']['img_max_height'] = 5000;    // максимальная высота загружаемых изображений в пикселях
 $config['view']['img_max_size_url'] = 500;    // максимальный размер картинки в kB для загрузки по URL
